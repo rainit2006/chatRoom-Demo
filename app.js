5433a0bd7bd24d261fd76e793489068cf7195c0c
unknown <9004007021@JPC20135658.jp.sony.com>
2016-06-10 13:31:38 +0900

    this is a first version

---
+++ app.js
@@ -0,0 +1,43 @@
+var http = require("http");
+var socketio = require("socket.io");
+var fs = require("fs");
+
+var server = http.createServer(function(req, res) {
+     res.writeHead(200, {"Content-Type":"text/html"});
+     var output = fs.readFileSync("./index.html", "utf-8");
+     res.end(output);
+}).listen(process.env.VMC_APP_PORT || 8080);
+
+var io = socketio.listen(server);
+
+// ユーザ管理ハッシュ
+var userHash = {};
+
+io.sockets.on("connection", function (socket) {
+
+  // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
+  socket.on("connected", function (name) {
+    var msg = name + "が入室しました";
+    userHash[socket.id] = name;
+    io.sockets.emit("S_to_C_message", {value: msg});
+  });
+
+  // メッセージ送信（送信者にも送られる）
+  socket.on("C_to_S_message", function (data) {
+    io.sockets.emit("S_to_C_message", {value:data.value});
+  });
+
+  // ブロードキャスト（送信者以外の全員に送信）
+  socket.on("C_to_S_broadcast", function (data) {
+    socket.broadcast.emit("S_to_C_message", {value:data.value});
+  });
+
+  // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
+  socket.on("disconnect", function () {
+    if (userHash[socket.id]) {
+      var msg = userHash[socket.id] + "が退出しました";
+      delete userHash[socket.id];
+      io.sockets.emit("S_to_C_message", {value: msg});
+    }
+  });
+});
